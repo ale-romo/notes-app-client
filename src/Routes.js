5b0476366bd24d55599d83422533354abb206871
Romo, Alejandro <alejandroromo_martinez@intuit.com>
2018-09-07 13:09:35 -0700

    created about page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import About from "./containers/About";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
@@ -12,7 +13,8 @@ import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/" exact component={About} props={childProps} />
+    <AppliedRoute path="/home" exact component={Home} props={childProps} />
     <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
     <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
     <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
---
+++ src/containers/About.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import "./About.css";
+import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
+
+export default class About extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      notes: []
+    };
+  }
+
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await this.notes();
+      this.setState({ notes });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  notes() {
+    return API.get("notes", "/notes");
+  }
+
+
+
+  render() {
+    return (
+      <div className="About ">
+        <p>
+          This is the about page now
+        </p>
+      </div>
+    );
+  }
+}
